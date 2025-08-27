0df834e536ee4ed0fb6906af6ad9fb30212ad06d
John Doe <johndoe@email.com>
2025-08-27 07:41:06 +0100

    feat: add environment configuration and settings for FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
 from routers import story, job
 
 
@@ -14,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
